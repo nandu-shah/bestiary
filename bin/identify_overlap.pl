45d7bb9344d5ef0c6de4f8886182791ae0d12666
Nandu Shah <nandu.shah@gmail.com>
2019-03-24 21:41:40 -0400

    emit only width or height as requested via -w or -g

--- bin/identify_overlap.pl
+++ bin/identify_overlap.pl
@@ -7,11 +7,13 @@ use Getopt::Std;
 use Image::Magick;
 
 my %opts;
-getopts('n:p:', \%opts);
+getopts('whn:p:', \%opts);
 
 $opts{p} //= 0.1;
 
 die "provide dimension with -n\n" unless defined $opts{n};
+die "provide exactly one of -w and -h\n"
+  unless defined $opts{w} xor defined $opts{h};
 
 my $file = shift || die "filename required\n";
 my $image = Image::Magick->new();
@@ -21,9 +23,5 @@ $image->Read($file);
 my $width  = $image->Get('width');
 my $height = $image->Get('height');
 
-my $overlap_w = sprintf('%.0f', $width * $opts{p} / $opts{n});
-my $overlap_h = sprintf('%.0f', $height * $opts{p} / $opts{n});
-
-print $width, 'x', $height, "\n";
-print "overlap_w=$overlap_w\n";
-print "overlap_h=$overlap_h\n";
+printf("%.0f\n", $width * $opts{p} / $opts{n}) if $opts{w};
+printf("%.0f\n", $height * $opts{p} / $opts{n}) if $opts{h};
